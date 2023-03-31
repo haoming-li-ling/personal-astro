621118d0f619bdcdc7c54e0855beef71cce2098a
Haoming Li <haomingli@Haomings-MacBook-Pro.local>
2023-03-31 02:38:38 -0500

    gh local config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://haomingli-ling.github.io',
+  base: '/personal-astro',
+  integrations: [mdx(), sitemap()],
 });
